b4e9d22ec4ff701236efdb6dc75a0375adf76c67
lizand001 <152097538+lizand001@users.noreply.github.com>
2026-06-10 13:39:30 -0500

    cambios titulo

--- kaza/settings.py
+++ kaza/settings.py
@@ -78,9 +78,9 @@ WSGI_APPLICATION = 'kaza.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'kazabd',
+        'NAME': 'kazadb',
         'USER': 'postgres',
-        'PASSWORD': 'root',
+        'PASSWORD': '123456',
         'HOST': 'localhost',
         'PORT': '5432',
     }
