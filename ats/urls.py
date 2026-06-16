972881e17964ee0d50d00a548dffb54507010430
camilatuso9119-coder <camilatuso9119@utc.edu.ec>
2026-06-16 13:58:40 -0500

    organicé las rutas con public y private

--- ats/urls.py
+++ ats/urls.py
@@ -1,14 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
-from dashboard.views import dashboard_view
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', RedirectView.as_view(url='/private/admin/', permanent=False)),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('', dashboard_view, name='dashboard'),
-    path('inventory/', include('inventory.urls')),
-    path('production/', include('production.urls')),
-    path('costs/', include('costs.urls')),
-    path('sales/', include('sales.urls')),
-    path('users/', include('users.urls')),
+    path('', include('public.urls')),
+    path('private/', include('private.urls')),
+    path('dashboard/', RedirectView.as_view(url='/private/', permanent=False)),
+    path('users/', RedirectView.as_view(url='/private/users/', permanent=False)),
+    path('inventory/', RedirectView.as_view(url='/private/inventory/', permanent=False)),
+    path('production/', RedirectView.as_view(url='/private/production/', permanent=False)),
+    path('costs/', RedirectView.as_view(url='/private/costs/', permanent=False)),
+    path('sales/', RedirectView.as_view(url='/private/sales/', permanent=False)),
 ]
