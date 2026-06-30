624cfc42122a9bfca278615006a769a0206d5af3
edson123 <edson.venegas5865@utc.edu.ec>
2026-06-30 09:41:07 -0500

    docs: documentar archivos estáticos

--- ats/settings.py
+++ ats/settings.py
@@ -120,7 +120,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
-
+# ==========================================
+# Archivos estáticos
+# ==========================================
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
